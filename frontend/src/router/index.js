3d6643cfb82429973cd187bc7fad11de66b5f25d
jbernuy <jorgebernuy0@gmail.com>
2020-10-19 15:22:57 -0500

    Vista home

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import P404 from '../views/P404';
 import tokenMiddleware from '../middlewares/tokenMiddleware';
+import userMiddleware from "../middlewares/userMiddleware";
 import userRoutes from "./userRoutes";
 import loginRoutes from "./loginRoutes";
 
@@ -17,6 +18,21 @@ const routes = [{
     },
     loginRoutes,
     userRoutes,
+    {
+        path: "/viewer/:aulaId/:cursoId/:tareaId",
+        component: () =>
+            import ('../views/ViewerCourse'),
+        children: [{
+            path: '/viewer/:aulaId/:cursoId/:tareaId',
+            name: 'Tarea',
+            component: () =>
+                import ('../views/Task'),
+            beforeEnter: userMiddleware
+        }, ],
+        meta: {
+            autenticate: true
+        },
+    },
 ];
 
 const router = new VueRouter({
--- frontend/src/router/userRoutes.js
+++ frontend/src/router/userRoutes.js
@@ -1,17 +1,73 @@
 import userMiddleware from "../middlewares/userMiddleware";
+import Home from '../views/Home';
 
 export default {
     path: "/",
-    name: "Home",
-    component: () =>
-        import ("../views/Home"),
+    component: Home,
     children: [{
-        path: '/cursos',
-        name: 'Cursos',
-        component: () =>
-            import ('../views/Courses'),
-        beforeEnter: userMiddleware
-    }, ],
+            path: '/',
+            name: 'Stats',
+            component: () =>
+                import ('../views/Stats'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/usuarios',
+            name: 'Users',
+            component: () =>
+                import ('../views/Users'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/aulas',
+            name: 'Classrooms',
+            component: () =>
+                import ('../views/Classrooms'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/aulas/:aulaId',
+            name: 'Classroom',
+            component: () =>
+                import ('../views/Classroom'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/aulas/:aulaId/:cursoId',
+            name: 'Classroom-course',
+            component: () =>
+                import ('../views/Course'),
+            beforeEnter: userMiddleware,
+            children: [{
+                path: '/aulas/:aulaId/:cursoId/:tareaId',
+                name: 'Classroom-course-task',
+                component: () =>
+                    import ('../views/ResumeTask'),
+                beforeEnter: userMiddleware
+            }, ]
+        },
+        {
+            path: '/grados',
+            name: 'Degrees',
+            component: () =>
+                import ('../views/Degrees'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/cursos',
+            name: 'Courses',
+            component: () =>
+                import ('../views/Courses'),
+            beforeEnter: userMiddleware
+        },
+        {
+            path: '/perfil',
+            name: 'Profile',
+            component: () =>
+                import ('../views/Profile'),
+            beforeEnter: userMiddleware
+        },
+    ],
     meta: {
         autenticate: true
     },
