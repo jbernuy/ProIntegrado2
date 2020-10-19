150aafff519046a0b2299467f397b1fc6c556eb9
jbernuy <jorgebernuy0@gmail.com>
2020-10-19 15:34:50 -0500

    Vistas listado

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,9 +1,22 @@
 <template>
-  
+  <div>
+    <FormRegister :isCreate="false" :userEdited="user" />
+  </div>
 </template>
 
 <script>
+import FormRegister from "@/components/forms/FormRegister";
+import { mapState } from "vuex";
+
 export default {
+    components: {
+        FormRegister,
+    },
+    computed: {
+        ...mapState({
+            user: state => state.user,
+        }),
+    },
 
 }
 </script>
